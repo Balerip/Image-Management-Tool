760120ca96d0f67984d1ddd527039839458559dd
panchamibaleri <panchami.baleri97@gmail.com>
2025-10-16 16:21:54 -0700

    removing project files

--- FinalProject/src/main/java/com/example/finalprojectjava/ImageConverterFactory.java
+++
@@ -1,9 +0,0 @@
-package com.example.finalprojectjava;
-
-/*Interface for ImageConverter This interface declares a method createImageConverter()
- which is responsible for creating an instance of the ImageConverter.*/
-
-// Interface for ImageConverterFactory
-public interface ImageConverterFactory {
-    ImageConverter createImageConverter();
-}
