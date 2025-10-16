d6ef224398d4777ad5d6010251872f2755892d59
panchamibaleri <panchami.baleri97@gmail.com>
2025-10-16 16:20:09 -0700

    adding project files

---
+++ FinalProject/src/main/java/com/example/finalprojectjava/ImageConverterFactory.java
@@ -0,0 +1,9 @@
+package com.example.finalprojectjava;
+
+/*Interface for ImageConverter This interface declares a method createImageConverter()
+ which is responsible for creating an instance of the ImageConverter.*/
+
+// Interface for ImageConverterFactory
+public interface ImageConverterFactory {
+    ImageConverter createImageConverter();
+}
